d5ea7d9887fc9bfcdd36af9aeef616ce795f2928
Meenaakshir <meenaakshivra@gmail.com>
2020-12-10 08:47:29 -0500

    adding form validation code

---
+++ formapp/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BasicForm from "./components/BasicForm.js";
+
+function App() {
+  return (
+    <div className="App">
+      <BasicForm />
+    </div>
+  );
+}
+
+export default App;
